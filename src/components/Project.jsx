d156412f859f0dcda7c25fc3a32d19fd5ad122e8
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2023-04-29 12:28:37 +0530

    Add files via upload

---
+++ src/components/Project.jsx
@@ -0,0 +1,223 @@
+import React from "react";
+// import i1 from "../components/image/i1.png"
+
+import imgport from "../components/image/imgport.png"
+
+import imgmovieapp from "../components/image/imgmovieapp.png"
+import imag1 from "../components/image/imag1.png"
+import imag2 from "../components/image/imag2.png"
+import imag3 from "../components/image/imag3.png"
+import imag4 from "../components/image/imag4.png"
+import imag5 from "../components/image/imag5.png"
+import imag6 from "../components/image/imag6.png"
+import imag7 from "../components/image/imag7.png"
+import imag8 from "../components/image/imag8.png"
+import imag9 from "../components/image/imag9.png"
+import imag10 from "../components/image/imag10.png"
+import imag11 from "../components/image/imag11.png"
+import imag12 from "../components/image/imag12.png"
+import imag13 from "../components/image/imag13.png"
+import imag14 from "../components/image/imag14.png"
+import imag15 from "../components/image/imag15.png"
+import imag16 from "../components/image/imag16.png"
+import imag17 from "../components/image/imag17.png"
+const Project=()=>{
+    const Projects=[
+        {
+            id:0,
+            project_name:"LinkedIn Clone",
+            src:imag16,
+            githublink:"https://github.com/jagtapnimisha2/LinkedIn-Clone---React-Project---kyiq4msruph7",
+            link:"https://glowing-tapioca-25d26b.netlify.app/"
+
+
+        },
+        {
+            id:1,
+            project_name:"Portfolio",
+            src:imgport, 
+            githublink:"https://github.com/jagtapnimisha2/portfolio--",
+            link:"https://stirring-axolotl-4e91ac.netlify.app/"
+         
+        },
+        {
+            id:2,
+            project_name:"Movie App",
+            src:imgmovieapp,
+            githublink:"https://github.com/jagtapnimisha2/Movie_App",
+            link:"https://main--willowy-donut-81fdb4.netlify.app/"
+
+        },
+        {
+            id:3,
+            project_name:"IOT Based Smart Garbage Monitoring System",
+            src:imag17,
+            githublink:"https://github.com/jagtapnimisha2/Garbage_Bin__Major_Project",
+            link:"https://smartgarbage-f4f3b.web.app"
+         
+        },
+        {
+            id:4,
+            project_name:"ineuron-assignment--1",
+            src:imag15,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment--15",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment--15/"
+
+            
+        },
+        {
+            id:5,
+            project_name:"ineuron-assignment--2",
+            src:imag14,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment--14",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment--14/"
+           
+        },
+        {
+            id:6,
+            project_name:"ineuron-assignment--3",
+            src:imag13,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment--13",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment--13/"
+        
+        },
+        {
+            id:7,
+            project_name:"ineuron-assignment--4",
+            src:imag12,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment--12",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment--12/"
+ 
+        },
+        {
+            id:8,
+            project_name:"ineuron-assignment--5",
+            src:imag11,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment--11",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment--11/"
+    
+        },
+        {
+            id:9,
+            project_name:"ineuron-assignment--6",
+            src:imag10,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment--10",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment--10/"
+
+        },
+        {
+            id:10,
+            project_name:"ineuron-assignment--7",
+            src:imag9,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-09",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment-09/"
+        },
+        {
+            id:11,
+            project_name:"ineuron-assignment--8",
+            src:imag8,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-08",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment-08/"
+       
+        },
+        {
+            id:12,
+            project_name:"ineuron-assignment--9",
+            src:imag7,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-07",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment-07/"
+            
+        },
+        {
+            id:13,
+            project_name:"ineuron-assignment--10",
+            src:imag6,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-06",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment-06/"
+           
+        },
+        {
+            id:14,
+            project_name:"ineuron-assignment--11",
+              src:imag5,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-05",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment-05/"
+     
+        },
+        {
+            id:15,
+            project_name:"ineuron-assignment--12",
+            src:imag4,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-04",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment-04/"
+          
+        },
+        {
+            id:16,
+            project_name:"ineuron-assignment--13",
+            src:imag3,  
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-03",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment-03/"
+         
+        },
+        {
+            id:17,
+            project_name:"ineuron-assignment--14",
+            src:imag2,
+            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-02",
+            link:"https://jagtapnimisha2.github.io/ineuron-assignment-02/"
+
+        },
+        {
+            id:18,
+            project_name:"ineuron-assignment--15",
+            src:imag1,
+            githublink:"https://github.com/jagtapnimisha2/Ineuron_assignment-01",
+            link:"https://jagtapnimisha2.github.io/Ineuron_assignment-01/"
+            
+        }
+    ]
+    return(
+ <div
+    name="Project"
+    className="bg-gradient-to-b from-black to-gray-800 w-full h-full
+    text-white md:h-screen lg:h-full
+    ">
+    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full
+     
+    ">
+        <div className="pb-8">
+            <p className="text-4xl font-bold inline  border-b-4 border-gray-500">Projects</p>
+            <p className="py-6">Check out some of my work right here</p>
+        </div>
+           
+       
+
+            <div className="grid sm:grid-cols-2 md:grid-cols-3  text-center gap-8 px-12 sm:px-0">
+            {
+         
+                Projects.map(({id,project_name,src,githublink,link})=>(
+                
+                <div key={id} className="shadow-md shadow-gray-600 rounded-lg ">
+                <br></br>
+                    <h3>{project_name}</h3>
+                    <br></br>
+                 <a  href={link} className=" duration-200 hover:scale-105">
+                    <img src={src} alt="this is a image"
+                    className="rounded-md duration-200 hover:scale-105"
+                    />
+                </a>
+                <div className="flex items-center justify-center">
+                    <a href={githublink} className="w-1/3 py-3 text-center m-2 duration-200 hover:scale-105">Code</a>
+                </div>
+            </div>
+        
+            
+            ))
+        }
+        </div>
+    </div>
+ </div>
+    );
+};
+export default Project;
